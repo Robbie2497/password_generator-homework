d36187b5f194e72508c5712a09d2d1693313c88d
Robert McGrew <robbie2497@gmail.com>
2020-03-24 21:55:32 -0600

    updating homework

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
- // Assignment Code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var password = document.querySelector("#password");
-
-
-let char = ['abcdefghijklmnopqrstuvwxyz'];
-let num= ["0123456789"]
-let sym = ["!@#$%^&*-_+"];
-
-let passwordSize = prompt("How many characters would you like in your password?");
-
 
 // Write password to the #password input
 function writePassword() {
@@ -17,24 +9,60 @@ function writePassword() {
 
 
   passwordText.value = password;
-  writePassword.vaule = passwordText.value;
+  writePassword.value = passwordText.value;
 
 }
 
+function generatePassword() {
+  let pass = [];
+  let char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let sym = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+"];
 
+  let passwordSize = prompt("How many characters between 8 and 128 would you like in your password?");
+  let upperCase = confirm("Would you like upper case letters in your password?");
+  let numbers = confirm("Would you like numbers in your password?");
+  let symbols = confirm("Would you like symbols in your password?");
 
+  let count = JSON.parse(passwordSize);
+  console.log(count);
+  console.log(pass);
 
+  if (upperCase) {
+    pass = pass.concat(big);
+  }
+  if (numbers) {
+    pass = pass.concat(num);
+  }
+  if (symbols) {
+    pass = pass.concat(sym);
+  }
+  if (pass.length === 0) {
+    pass = pass.concat(char)
+  }
 
+  console.log(pass);
 
+  let newPass = "";
 
+  for (i = 0; i < count; i++) {
+    //newPass = newPass.(pass)
+    let randomIndex = Math.floor(Math.random() * pass.length);
+    newPass += pass[randomIndex];
+  }
+  console.log(newPass);
 
+  return newPass;
+
+}
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-//"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-//"!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "+"
+
+
+
 
 
